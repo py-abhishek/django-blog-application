f5453445bd4a366ded70b8f595ee93e1b01863a3
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-01-25 20:14:05 +0000

    home page created using html, css and dynamic blogs section intoduced

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+blogs = [
+    {
+        'title': 'This is my first blog',
+        'content': 'This blog is about django, we will explore the feature, uses, and scope in django in this...'
+    },
+    {
+        'title': 'This is my second blog',
+        'content': 'This blog is about django, we will explore the feature, uses, and scope in django in this...'
+    },
+    {
+        'title': 'This is my third blog',
+        'content': 'This blog is about django, we will explore the feature, uses, and scope in django in this...'
+    }
+]
+
 
 # Create your views here.
+# / page
+def index(request):
+    return render(request, "blog/index.html",{
+            'blogs': blogs
+        })
+
+# /blogs page
+# def blogs(request):
+#     return render(request, "blog/blogs.html")
