a0d75880e935023497d1d0f6e64ccdf1daef8bf7
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-01-25 23:56:38 +0000

    Sort latest posts for home page

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ blogs = [
     {
         'slug': 'my-first-blog-post',
         'author': 'Abhishek Chaudhary',
-        'date': date(2026, 1, 26),
+        'date': date(2026, 1, 21),
         'title': 'This is my first blog',
         'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
         'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
@@ -17,7 +17,7 @@ blogs = [
         'slug': 'my-second-blog-post',
         'author': 'Abhishek Chaudhary',
         'date': date(2026, 1, 26),
-        'title': 'This is my first blog',
+        'title': 'This is my second blog',
         'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
         'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
     },
@@ -25,18 +25,32 @@ blogs = [
         'slug': 'my-third-blog-post',
         'author': 'Abhishek Chaudhary',
         'date': date(2026, 1, 26),
-        'title': 'This is my first blog',
+        'title': 'This is my third blog',
+        'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
+        'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
+    },
+    
+    {
+        'slug': 'my-third-blog-post',
+        'author': 'Abhishek Chaudhary',
+        'date': date(2026, 1, 25),
+        'title': 'This is my forth blog',
         'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
         'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
     },
 ]
 
+def get_date(post):
+    return post['date']
+
 
 # Create your views here.
 # / page
 def index_view(request):
+    sorted_posts = sorted(blogs, key=get_date)
+    latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html",{
-            'blogs': blogs
+            'blogs': latest_posts
         })
 
 # /blogs page
