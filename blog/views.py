ca8cdd8f1371eb39b2bcd843f2444acf427407f9
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-01-25 23:31:12 +0000

    Blog detail page added, content loading dynamically

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,34 @@
+from datetime import date
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 blogs = [
     {
+        'slug': 'my-first-blog-post',
+        'author': 'Abhishek Chaudhary',
+        'date': date(2026, 1, 26),
         'title': 'This is my first blog',
-        'content': 'This blog is about django, we will explore the feature, uses, and scope in django in this...'
+        'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
+        'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
     },
     {
-        'title': 'This is my second blog',
-        'content': 'This blog is about django, we will explore the feature, uses, and scope in django in this...'
+        'slug': 'my-second-blog-post',
+        'author': 'Abhishek Chaudhary',
+        'date': date(2026, 1, 26),
+        'title': 'This is my first blog',
+        'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
+        'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
     },
     {
-        'title': 'This is my third blog',
-        'content': 'This blog is about django, we will explore the feature, uses, and scope in django in this...'
-    }
+        'slug': 'my-third-blog-post',
+        'author': 'Abhishek Chaudhary',
+        'date': date(2026, 1, 26),
+        'title': 'This is my first blog',
+        'excerpt': 'This blog is about django, we will explore the feature, uses, and scope in django in this...',
+        'content': 'Reiciendis nulla itaque ipsum a non, rem temporibus similique at quam, officiis delectus amet quam laborum sequi ex neque, quia veritatis nulla, sit ipsa nesciunt dolorem nam provident laudantium aut odio assumenda quasi. Nesciunt animi atque assumenda, placeat architecto non velit debitis molestiae nihil, suscipit mollitia officia repellendus, quisquam velit dolore sunt rerum, illum rem voluptates ex eos animi? Ea corrupti porro distinctio quisquam consequuntur eligendi dolor, dolorem cumque nam reprehenderit repudiandae itaque ratione odit similique eveniet, nulla aspernatur quibusdam nam omnis, hic eius quam expedita?'
+    },
 ]
 
 
@@ -36,5 +50,9 @@ def about_view(request):
     return render(request, "blog/about.html")
 
 # /blog/post_details page
-def blog_detail(request):
-    return render(request, "blog/blog_detail.html")
+def blog_detail(request, slug):
+    for blog in blogs:
+        if blog['slug'] == slug:
+            return render(request, "blog/blog_detail.html", {
+                'blog_detail': blog
+            })
