e59b3072b3bedf20864de465a5bb55d4444fc23c
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-01-29 19:32:57 +0000

    Data Models Added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Author(models.Model):
+    f_name = models.CharField(max_length=50)
+    l_name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField()
