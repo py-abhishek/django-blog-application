4c1081b22d63eb26a668c42e4df2a002e6a2e4f3
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-02-08 19:52:00 +0000

    comments model added

--- blog/models.py
+++ blog/models.py
@@ -25,8 +25,14 @@ class Post(models.Model):
     slug = models.SlugField(unique=True, db_index=True)
     excerpt = models.CharField(max_length=200)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    image = models.ImageField(upload_to="blog_images/")
+    image = models.ImageField(upload_to="blog_images/", default="blog_images/7090544.jpg")
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
