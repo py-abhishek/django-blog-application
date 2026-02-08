b446bdbb334247aebc2f6e1455a7e576084c0b78
Abhishek Chaudhary <abhishekchaudhary0100@gmail.com>
2026-02-08 19:00:01 +0000

    added class based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index_view, name="index-page"),
-    path("blog", views.blogs_view, name="blog-page"),
-    path("about", views.about_view, name="about-page"),
-    path("blog/<slug:slug>", views.blog_detail, name="blog-detail")
+    path("", views.IndexView.as_view(), name="index-page"),
+    path("blog", views.BlogsView.as_view(), name="blog-page"),
+    path("about", views.AboutView.as_view(), name="about-page"),
+    path("blog/<slug:slug>", views.BlogDetailView.as_view(), name="blog-detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,35 +2,47 @@ from datetime import date
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
 from blog.models import Post
 import logging
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+
 # / page
-def index_view(request):
-    posts = Post.objects.filter(tags__in=[0, 1]).order_by('-date')[:3] # tags = 0 filter by single tag; tags__in=[0, 1] filter by multiple tags
-    # posts = Post.objects.all().order_by("-date")[:3] # will create database query to get only 3 posts
-    return render(request, "blog/index.html", {
-        "blogs": posts
-    })
+class IndexView(TemplateView):
+    template_name = "blog/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = Post.objects.filter(tags__in=[0, 1]).order_by('-date')[:3] # tags = 0 filter by single tag; tags__in=[0, 1] filter by multiple tags
+        context["blogs"] = posts
+        return context
+
 
 # /blogs page
-def blogs_view(request):
-    posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/blog.html",{
-            'blogs': posts
-        })
+class BlogsView(ListView):
+    model = Post
+    template_name = "blog/blog.html"
+    context_object_name = "blogs"
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        blogs = base_query.order_by("-date")
+        return blogs
+
 
 # /about page
-def about_view(request):
-    return render(request, "blog/about.html")
+class AboutView(TemplateView):
+    template_name = "blog/about.html"
+
 
 # /blog/post_details page
-def blog_detail(request, slug):
-    # blog = Post.objects.get(slug=slug)
-    blog = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/blog_detail.html", {
-        'blog_detail': blog
-    })
+class BlogDetailView(DetailView):
+    template_name = "blog/blog_detail.html"
+    model = Post
+    context_object_name = "blog_detail"
